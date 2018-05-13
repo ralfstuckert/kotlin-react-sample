43130939a723d8e550440bd18039ca94eca7175b
Ralf Stuckert <ralf.stuckert@googlemail.com>
2018-05-13 15:21:25 +0200

    Type for lodash got lost

--- src/lodash/Lodash.kt
+++ src/lodash/Lodash.kt
@@ -1,7 +1,7 @@
 package lodash
 
 @JsModule("lodash")
-external val lodash: dynamic
+external val lodash: Lodash
 
 external interface Lodash {
     fun <K,V> debounce(functionToDebounce: (K) -> V, debounceMillis: Int): (K) -> V
